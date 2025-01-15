67494a9d28d0f3130ba9e3c876bb574bf83160ff
unknown <mounaettalbi@gmail.com>
2025-01-15 22:14:58 +0100

    commit changes

--- __manifest__.py
+++ __manifest__.py
@@ -1,15 +1,21 @@
 {
- 'name': 'Gestion des Commandes',
- 'version': '1.0',
- 'summary': 'Module de gestion des commandes client',
- 'category': 'Sales',
- 'author': 'ELMECHHOURI ETTALBI',
- 'website': 'https://edu-ladywear.odoo.com',
- 'depends': ['base', 'sale'],
- 'data': [
- 'views/views.xml',
- ],
- 'installable': True,
- 'application': True,
- 'auto_install': False,
+    'name': 'Gestion Commandes',
+    'version': '1.0',
+    'author': 'Votre Nom',
+    'category': 'Custom',
+    'summary': 'Module pour gérer les commandes des clients',
+    'description': """
+Gestion des commandes pour les clients, incluant :
+- Création de commandes
+- Suivi de l’état des commandes
+    """,
+    'depends': ['base'],
+    'data': [
+        'security/security.xml',
+        'security/ir.model.access.csv',
+        'views/gestion_commandes_views.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
 }
