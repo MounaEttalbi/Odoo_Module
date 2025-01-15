c0983a88611e3ccf3678cfec81f1103d303c2b2c
hajar_elmechhouri <hajarelmech@gmail.com>
2025-01-15 12:20:33 +0100

    add configuration

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+{
+ 'name': 'Gestion des Commandes',
+ 'version': '1.0',
+ 'summary': 'Module de gestion des commandes client',
+ 'category': 'Sales',
+ 'author': 'ELMECHHOURI ETTALBI',
+ 'website': 'https://edu-ladywear.odoo.com',
+ 'depends': ['base', 'sale'],
+ 'data': [
+ 'views/views.xml',
+ ],
+ 'installable': True,
+ 'application': True,
+}
